b665b093faf04c652aa07ab8e0e44222806d233c
Denyme24 <namanraj24@outlook.com>
2024-11-06 01:32:18 +0530

    some ui changes

--- app/about/page.js
+++ app/about/page.js
@@ -3,19 +3,19 @@ import React from "react";
 
 const AboutUs = () => {
   return (
-    <div className="container mx-auto py-10">
-      <div className="bg-white shadow-md rounded-lg p-6">
-        <h1 className="text-3xl font-bold text-center text-black mb-6">About Us</h1>
-        <p className="text-gray-700 text-lg mb-4">
+    <div className="flex justify-center items-center h-[80vh] overflow-hidden">
+      <div className="max-w-2xl w-[50vw] p-6 overflow-hidden">
+        <h1 className="text-4xl font-extrabold text-center mb-6">About Us</h1>
+        <p className="text-lg mb-4">
           Welcome to ZapURL, your number one source for shortening URLs. We're dedicated to providing you the very best of URL shortening services, with an emphasis on ease of use, reliability, and efficiency.
         </p>
-        <p className="text-gray-700 text-lg mb-4">
-          Founded in 2023, ZapURL has come a long way from its beginnings. When we first started out, our passion for making the web a more accessible place drove us to start our own business.
+        <p className="text-lg mb-4">
+          Founded in 2024, ZapURL has come a long way from its beginnings. When we first started out, our passion for making the web a more accessible place drove us to start our own business.
         </p>
-        <p className="text-gray-700 text-lg mb-4">
+        <p className="text-lg mb-4">
           We hope you enjoy our service as much as we enjoy offering it to you. If you have any questions or comments, please don't hesitate to contact us.
         </p>
-        <p className="text-gray-700 text-lg mb-4">
+        <p className="text-lg mb-4">
           Sincerely,
           <br />
           The ZapURL Team
--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,11 @@
 import React from "react";
 
 const Footer = () => {
+    const currentYear = new Date().getFullYear();
     return (
         <footer className="footer bg-gray-800 text-white py-4 mt-auto">
             <div className="container mx-auto text-center">
-                <p>&copy; 2023 Your Website. All rights reserved.</p>
+                <p>&copy; {currentYear} ZapURL. All rights reserved.</p>
                 <div className="mt-2">
                     <a href="/" className="text-gray-400 hover:text-white mx-2">
                         Privacy Policy
